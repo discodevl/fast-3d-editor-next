85cd86e4843960c4cb1e1378291ee6157a914ce4
discodevl <dcutro@disroot.org>
2026-09-03 00:12:57 +0200

    export desc

--- src/actions/Export.tsx
+++ src/actions/Export.tsx
@@ -26,16 +26,19 @@ function Export() {
   }
 
   return (
-    <div className="flex flex-col items-center gap-4 mt-6.25">
+    <div className="flex flex-col items-center gap-2 md:mt-6.25 mt-3">
       {/* <button className="mb-5 p-2 rounded-lg text-white w-full bg-surface border border-app-border cursor-pointer hover:opacity-50" onClick={discardHandler}>Discard Model</button> */}
-      <h2 className="font-bold">Export your glb model</h2>
-      <p>Download your modified model, leaving the page without exporting your model will result in the loss of your modifications</p>
+      <h2 className="font-bold">Download your model</h2>
       <button
-        className="mb-5 p-2 rounded-lg text-white w-full bg-surface border border-app-border cursor-pointer hover:opacity-50"
+        className="p-2 rounded-lg text-white w-full bg-surface border border-app-border cursor-pointer hover:opacity-50"
         onClick={saveHandler}
       >
-        Export
+        Download
       </button>
+      <p className="text-[10px]">
+        Leaving the page without exporting
+        will result in the loss of your changes.
+      </p>
     </div>
   );
 }
